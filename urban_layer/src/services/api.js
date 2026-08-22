2b0ffe9613c3e4dfe9877b77d1f7ffbaffc17351
abhinav-pratapsingh <abhinavpratap6142@gmail.com>
2026-08-22 12:22:26 +0530

    api change

--- urban_layer/src/services/api.js
+++ urban_layer/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  // baseURL: "http://localhost:4000/api",
+  baseURL: "http://187-127-135-158.traefik.me/api",
   headers: {
     "Content-Type": "application/json",
   },
